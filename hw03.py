08939df490da1d9de75c0bc40f3cc5143d9cf559
WesleyCh3n <yoyochen0530@gmail.com>
2019-11-06 18:59:55 +0800

    update

--- hw03.py
+++ hw03.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('/Users/YoChen/Documents/GitHub/Img-Processing/HW04/test.jpg',0)
+img = cv2.imread('/home/y0ch3n/Documents/GitHub/Img-Processing/HW04/test.jpg',0)
+# img = cv2.imread('/Users/YoChen/Documents/GitHub/Img-Processing/HW04/test.jpg',0)
 print(img[200:250,200:250])
 img[200:250,200:250] = img[200:250,200:250]*0
 print(img[200:250,200:250])
