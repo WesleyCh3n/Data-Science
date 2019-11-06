8b349f6cdf1b7114f1bbfa8d01f3854d0b87e131
WesleyCh3n <yoyochen0530@gmail.com>
2019-11-06 15:56:10 +0800

    update

--- hw03.py
+++ hw03.py
@@ -9,8 +9,3 @@ import numpy as np
 
 
 img = cv2.imread('/Users/YoChen/Documents/GitHub/Img-Processing/HW04/test.jpg',0)
-cv2.imshow('a',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-print("joj")
