bd9aaa20c6cb557763e974198a2f4f8f398bad60
WesleyCh3n <yoyochen0530@gmail.com>
2019-11-07 01:14:20 +0800

    update hw03

--- HW03/hw03.py
+++ HW03/hw03.py
@@ -1,12 +1,20 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 import cv2
 import math as mt
 
-img = cv2.imread('/Users/YoChen/Documents/GitHub/Img-Processing/HW04/test.jpg',0)
-print(img.shape)
+img = cv2.imread('B.jpg',0)
+print(f"Image size is {img.shape}")
 U, S, V = np.linalg.svd(img)
-print(a.shape)
-print(b.shape)
-print(c.shape)
+print(f"U shape is {U.shape}, S shape is {S.shape}, S shape is {V.shape}")
+
+s = min(img.shape)
+val = [1, 0.8, 0.5, 0.2, 0.1, 0.05]
+nSingular = [mt.floor(s * val[i]) for i in range(len(val))]
+print(nSingular)
+plt.figure(figsize=[9.6, 7])
+for i in range(6):
+    low_rank = U[:, :nSingular[i]] @ np.diag(S[:nSingular[i]]) @ V[:nSingular[i], :]
+    plt.subplot(2,3,i+1), plt.imshow(low_rank, cmap = 'gray')
+    plt.axis('off'), plt.title(f"Image with {val[i]*100}% Singulars({nSingular[i]})", fontsize=10)
+plt.show()
