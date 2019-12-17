b777ea4eef48b7fad35187459f6daa9032d88d09
WesleyCh3n <yoyochen0530@gmail.com>
2019-12-18 00:25:25 +0800

    update

--- final_project/model.py
+++ final_project/model.py
@@ -1,7 +1,46 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('./london_merged.csv')
-print(df.describe())
-print(df.iloc[:,2])
-print(df)
+# regression
+def reg(x, y):
+    coeff = np.polyfit(x, y, 1)
+    arg1, arg2 = coeff
+    line = x * arg1 + arg2
+    return line, f'y = {int(arg1)}x + {int(arg2)}'
+
+# import data
+df = pd.read_csv('./london.csv')
+df2 = pd.read_csv('./DC.csv')
+Ld = df.to_numpy()
+DC = df2.to_numpy()
+print(df.columns)   # show the columns name
+print(f'Total {Ld.shape[0]} data')
+
+# preprocessing data / normalize
+Ld_cnt = Ld[:, 1].flatten().astype(float)
+Ld_t1 = Ld[:, 2].flatten().astype(float)
+Ld_t1 = (Ld_t1 - Ld_t1.min()) / (Ld_t1.max() - Ld_t1.min())
+Ld_t2 = Ld[:, 3].flatten().astype(float)
+Ld_t2 = (Ld_t2 - Ld_t2.min()) / (Ld_t2.max() - Ld_t2.min())
+Ld_hum = Ld[:, 4].flatten().astype(float)
+Ld_hum = Ld_hum/Ld_hum.max()
+Ld_wind = Ld[:, 5].flatten().astype(float)
+Ld_wind = Ld_wind/Ld_wind.max()
+Ld_weather = Ld[:, 6].flatten().astype(int)
+Ld_holiday = Ld[:,7].flatten().astype(int)
+Ld_weekend = Ld[:,8].flatten().astype(int)
+Ld_season = Ld[:,9].flatten().astype(int)
+
+name = ['Temperature', 'Feeling Temperature', 'Humidity', 'Wind Speed']
+color = ['#2af423', '#ff7400', '#e300ff', '#1eb5ff']
+Ld_data = [Ld_t1, Ld_t2, Ld_hum, Ld_wind]
+fig = plt.figure()
+for i in range(4):
+    trend_line, text = reg(Ld_data[i], Ld_cnt)
+    ax = fig.add_subplot(2, 2, i+1)
+    ax.scatter(Ld_data[i], Ld_cnt, c = color[i], s = 2, alpha = 0.3)  
+    ax.scatter(Ld_data[i], trend_line, c = 'r', s = 2, alpha = 1)  
+    ax.text(0.03, 0.95, text, fontname = 'Arial', fontsize = 10, weight='bold', transform=ax.transAxes, va='top')
+    ax.set_title(name[i])
+plt.show()
