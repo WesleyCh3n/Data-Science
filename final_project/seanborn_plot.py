c4b00462a3dd7463c23480eebc6481f5873916a1
WesleyCh3n <yoyochen0530@gmail.com>
2019-12-20 23:21:06 +0800

    update

--- final_project/seanborn_plot.py
+++ final_project/seanborn_plot.py
@@ -16,14 +16,16 @@ df.to_csv('normal.csv', index=False)
 
 df['date'] = pd.to_datetime(df['timestamp'])
 df.drop(columns =['timestamp'], inplace = True)
-df = df[['date', 'cnt', 't1', 't2', 'hum', 'wind_speed', 'weather_code', 'is_holiday', 'is_weekend', 'season']]
+# df = df[['date', 'cnt', 't1', 't2', 'hum', 'wind_speed', 'weather_code', 'is_holiday', 'is_weekend', 'season']]
 print(df)
 
 '''add year/month/day'''
 df['month'] = df['date'].dt.month
 df['year'] = df['date'].dt.year
 df['hr'] = df['date'].dt.hour
+df = df[['month', 'hr', 'cnt', 't1', 't2', 'hum', 'wind_speed', 'weather_code', 'is_holiday', 'is_weekend', 'season']]
 print(df)
+df.to_csv('normalize-addtime.csv', index=False)
 
 # print(df[df['month'] == 2]['cnt'].sum())
 # print([df[df['month'] == (i+1)]['cnt'].sum() for i in range(12)])
@@ -42,10 +44,10 @@ print(df)
 # ax = sn.boxplot(x='year', y='cnt', data=df)
 # plt.show()
 
-sn.set(color_codes=True)
-fig, axs = plt.subplots(2, 3)
-name = ['t1', 't2', 'hum', 'wind_speed', 'weather_code', 'season']
-for i in range(2):
-    for j in range(3):
-        chart = sn.regplot(name[2*i+j], 'cnt', df, scatter_kws={'s': 5, 'alpha':0.3}, line_kws={'color': 'red'}, ax=axs[i, j])
-plt.show()
+# sn.set(color_codes=True)
+# fig, axs = plt.subplots(2, 3)
+# name = ['t1', 't2', 'hum', 'wind_speed', 'weather_code', 'season']
+# for i in range(2):
+#     for j in range(3):
+#         chart = sn.regplot(name[2*i+j], 'cnt', df, scatter_kws={'s': 5, 'alpha':0.3}, line_kws={'color': 'red'}, ax=axs[i, j])
+# plt.show()
